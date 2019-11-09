13e75cf5a95bda5b3c1b907750fc6285aaa02092
Thophile <montemonttheophile@gmail.com>
2019-11-09 15:08:55 +0100

    feat: valid lik in navbar
    added valid link in navbar to help testing and debug

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="main")
+     * @Route("/", name="index")
      */
     public function index()
     {

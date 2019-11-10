2abe84451e4936709d8e0f520c45fc14731cc978
Samil ERDOGAN <samil.erdogan@outlook.com>
2019-11-10 20:37:48 +0100

    feat: Upload Image to an event+ connect if not

--- src/Controller/Event/EventController.php
+++ src/Controller/Event/EventController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Event;
 
 use App\Entity\Evenement;
+use App\Entity\Upload;
+use App\Entity\Photo;
 use App\Repository\EvenementRepository;
+use App\Form\UploadType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -23,11 +28,42 @@ class EventController extends AbstractController
     /**
     * @Route("/event/{id}", name="event_show")
     */
-    public function event_show(Evenement $event){
+    public function event_show(Evenement $event, Request $request, $id ){
+
+        $upload = new Upload();
+        $form = $this->createForm(UploadType::class, $upload);
         
+        $fileUrl = "";
+
+        $form->handleRequest($request);
         
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $file = $upload->getName();
+            $fileName = md5(uniqid()).'.'.$file->guessExtension();
+            $file->move($this->getParameter('upload_directory'), $fileName);
+            $upload->setName($fileName);
+
+            $photo = new Photo;
+            $photo->setUrl("../photos/".$fileName);
+            $photo->setauthor( $user = $this->getUser() );
+            $photo->setEvenement( $event );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($photo);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('event_show', [
+                'id' => $id,
+            ]);
+
+
+        }
+
         return $this->render('event/show_event.html.twig', [
-            'event' => $event
+            'event' => $event,
+            'form' => $form->createView(),
+            'fileUrl' => $fileUrl,
         ]);
     }
 }
---
+++ src/Form/UploadType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use App\Entity\Upload;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UploadType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', FileType::class, [
+                'label' => 'Choisissez une photo'
+            ])
+            
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Upload::class,
+        ]);
+    }
+}

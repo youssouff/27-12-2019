2b4f441f799e97704f530ba4e410e3c50a77bc64
Thophile <montemonttheophile@gmail.com>
2019-11-13 18:33:10 +0100

    feat: created api service
    api service is responsible of the communication with the node.js api that has acces to the national database

---
+++ src/Service/Api.php
@@ -0,0 +1,24 @@
+<?php 
+// src/Service/Api.php
+namespace App\Service;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class Api
+{
+    public function getUserFullName(int $id): string
+    {
+
+    }
+
+    public function login($mail, $password): ArrayCollection
+    {
+
+    }
+
+    public function register($mail, $password): boolval
+    {
+
+    }
+
+}
